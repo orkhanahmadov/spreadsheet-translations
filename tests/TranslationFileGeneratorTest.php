c79844c79a95069b7b11e61bc5af6b9a193174aa
Orkhan Ahmadov <hi@orkhan.dev>
2023-02-27 17:50:56 +0100

    WIP

---
+++ tests/TranslationFileGeneratorTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Orkhanahmadov\SpreadsheetTranslations\Tests;
+
+use Orkhanahmadov\SpreadsheetTranslations\TranslationFileGenerator;
+
+class TranslationFileGeneratorTest extends TestCase
+{
+    public function testGeneratesTranslationFiles(): void
+    {
+        $generator = new TranslationFileGenerator();
+
+        $generator->generate([
+            'en' => [
+                'file' => ['k' => 'v'],
+            ]
+        ]);
+
+        $this->assertFileExists($filepath = lang_path('en/file.php'));
+        $this->assertSame("<?php return [\r\n'k' => 'v'\r\n];", file_get_contents($filepath));
+        unlink($filepath);
+    }
+}
