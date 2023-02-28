859902e5310330f5ccd23d47f181ebe4b8c04aad
Orkhan Ahmadov <hi@orkhan.dev>
2023-02-28 20:24:59 +0100

    Fix `lang_path` backward compatibility

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,6 @@ declare(strict_types=1);
 if (! function_exists('lang_path')) {
     function lang_path(string $path = ''): string
     {
-        return resource_path($path);
+        return resource_path("lang/{$path}");
     }
 }
