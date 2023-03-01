b18a2e0e7719d4c42a418e9d3e267089bd1f7028
Orkhan Ahmadov <hi@orkhan.dev>
2023-03-01 12:04:52 +0100

    Update `lang_path` backward compatibility with Laravel 8

--- src/functions.php
+++ src/functions.php
@@ -5,6 +5,10 @@ declare(strict_types=1);
 if (! function_exists('lang_path')) {
     function lang_path(string $path = ''): string
     {
-        return resource_path("lang/{$path}");
+        if (! empty($path)) {
+            $path = 'lang' .DIRECTORY_SEPARATOR . $path;
+        }
+
+        return resource_path($path);
     }
 }
