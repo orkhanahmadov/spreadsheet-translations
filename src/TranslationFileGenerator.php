c4805e00e295545b070ecddadf3cb1fb584e3cfe
Orkhan Ahmadov <hi@orkhan.dev>
2023-03-01 12:29:40 +0100

    JSON file instead of PHP

--- src/TranslationFileGenerator.php
+++ src/TranslationFileGenerator.php
@@ -4,40 +4,13 @@ declare(strict_types=1);
 
 namespace Orkhanahmadov\SpreadsheetTranslations;
 
-use Illuminate\Support\Collection;
-
 class TranslationFileGenerator
 {
-    public function generate(string $locale, array $groups): void
+    public function generate(string $locale, array $translations): void
     {
-        $this->createLocaleFolderIsMissing($locale);
-
-        foreach ($groups as $filename => $translations) {
-            file_put_contents(
-                lang_path("{$locale}/{$filename}.php"),
-                $this->generateTranslationFileContents($translations)
-            );
-        }
-    }
-
-    protected function generateTranslationFileContents(array $translations): string
-    {
-        $output = Collection::make($translations)
-            ->map(fn (?string $translation) => str_replace("'", "\'", $translation ?? ''))
-            ->map(fn (?string $translation, string $key) => "'$key' => '{$translation}'")
-            ->join(",\r\n");
-
-        return "<?php return [\r\n{$output}\r\n];";
-    }
-
-    protected function createLocaleFolderIsMissing(string $locale): void
-    {
-        $folder = lang_path($locale);
-
-        if (file_exists($folder)) {
-            return;
-        }
-
-        mkdir($folder);
+        file_put_contents(
+            lang_path("{$locale}.json"),
+            json_encode($translations)
+        );
     }
 }
--- tests/TranslationFileGeneratorTest.php
+++ tests/TranslationFileGeneratorTest.php
@@ -8,60 +8,22 @@ use Orkhanahmadov\SpreadsheetTranslations\TranslationFileGenerator;
 
 class TranslationFileGeneratorTest extends TestCase
 {
-    protected TranslationFileGenerator $generator;
-    protected string $filepath;
-
     public function testGeneratesTranslationFiles(): void
     {
-        $this->assertFalse(file_exists($this->filepath));
+        $filepath = lang_path('en.json');
 
-        $this->generator->generate('en', [
-            'file' => ['k' => 'v'],
+        $this->assertFalse(file_exists($filepath));
+
+        (new TranslationFileGenerator())->generate('en', [
+            'identifier' => ['k' => 'v'],
         ]);
-
-        $this->assertTrue(file_exists($this->filepath));
-        $this->assertSame(
-            "<?php return [\r\n'k' => 'v'\r\n];",
-            file_get_contents($this->filepath)
-        );
-    }
-
-    public function testEscapesSingleQuiteCharacters(): void
-    {
-        $this->generator->generate('en', [
-            'file' => ['k' => "don't do it!"],
-        ]);
-
-        $this->assertSame(
-            "<?php return [\r\n'k' => 'don\'t do it!'\r\n];",
-            file_get_contents($this->filepath)
-        );
-    }
-
-    public function testCreatesFolderWhenDoesNotExist(): void
-    {
-        $this->assertFalse(file_exists($filepath = lang_path('fr')));
-
-        $this->generator->generate('fr', []);
 
         $this->assertTrue(file_exists($filepath));
-        rmdir($filepath);
-    }
+        $this->assertSame(
+            '{"identifier":{"k":"v"}}',
+            file_get_contents($filepath)
+        );
 
-    protected function setUp(): void
-    {
-        parent::setUp();
-
-        $this->generator = new TranslationFileGenerator();
-        $this->filepath = lang_path('en/file.php');
-    }
-
-    protected function tearDown(): void
-    {
-        if (isset($this->filepath) && file_exists($this->filepath)) {
-            unlink($this->filepath);
-        }
-
-        parent::tearDown();
+        unlink($filepath);
     }
 }
